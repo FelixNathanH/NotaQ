db4c078c5978d2f8a5a58e3391410ca6b2c40c29
FelixNathanH <felixnathan1608@gmail.com>
2023-06-12 16:26:29 +0700

    fix 1 error

--- NotaQ/Factory/NotaFactory.cs
+++ NotaQ/Factory/NotaFactory.cs
@@ -8,14 +8,14 @@ namespace NotaQ.Factory
 {
     public class NotaFactory
     {
-        public nota createNota(int id, string buyerName, string buyerPhoneNumber, DateTime buyDate, int employee_id, int totalPrice, int totalPayment, string paymentMethod)
+        public nota createNota(int id, string buyerName, string buyerPhoneNumber, DateTime buyDate, string employee_name, int totalPrice, int totalPayment, string paymentMethod)
         {
             nota newNota = new nota();
             newNota.Id = id;
             newNota.buyer_name = buyerName;
             newNota.buyer_phone_number = buyerPhoneNumber;
             newNota.buy_date = buyDate;
-            newNota.employee_id = employee_id;
+            newNota.employee_name = employee_name;
             newNota.total_price = totalPrice;
             newNota.total_payment = totalPayment;
             newNota.payment_method = paymentMethod;
